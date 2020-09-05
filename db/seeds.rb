84282fa71958a5a3604ebb9674d5946e2acc3409
Sharon Sheah <sharonsheah@gmail.com>
2020-09-06 04:11:59 +1000

    parse and seed ingredients

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,14 +1,15 @@
-puts "Creating ingredients..."
-Ingredient.create(name: "lemon")
-Ingredient.create(name: "ice")
-Ingredient.create(name: "mint leaves")
-Ingredient.create(name: "watermelon")
+puts "Cleaning database..."
 
-puts "Created #{Ingredient.count} ingredients"
+Ingredient.destroy_all
 
-# require 'json'
-# require 'open-uri'
+puts "Creating ingredients from API..."
+require 'json'
+require 'open-uri'
 
-# url = 'https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list'
-# ingredients_serialised = open(url).read
-# ingredient = JSON.parse(ingredients_serialised)
+url = 'https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list'
+ingredients_serialised = open(url).read
+ingredients = JSON.parse(ingredients_serialised)
+ingredients_arr = ingredients.first[1]
+ingredients_arr.each { |ingredient| Ingredient.create(name: ingredient["strIngredient1"]) }
+
+puts "Created #{Ingredient.count} ingredients from API"
