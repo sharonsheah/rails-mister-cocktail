970c56945eea21c1a98a95730326bb2d16b27906
Sharon Sheah <sharonsheah@gmail.com>
2020-09-06 03:33:45 +1000

    implemented responsive design

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_04_054940) do
+ActiveRecord::Schema.define(version: 2020_09_05_135158) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -40,6 +40,7 @@ ActiveRecord::Schema.define(version: 2020_09_04_054940) do
     t.string "name"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.text "description"
   end
 
   create_table "doses", force: :cascade do |t|
